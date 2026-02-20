3e82ee612c7372e6d455427ca8f386623a15898f
Leonardo <leonardops@gmail.com>
2026-02-19 22:02:35 -0300

    Refactor BuscaMotorista fields and add action listener

--- BuscaMotorista.java
+++ BuscaMotorista.java
@@ -11,15 +11,17 @@ import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class BuscaMotorista extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
+	private JTextField txtFieldNome;
+	private JTextField txtFieldNomeSoc;
+	private JTextField txtFieldCPF;
+	private JTextField txtFieldCNH;
 
 	/**
 	 * Launch the application.
@@ -50,65 +52,77 @@ public class BuscaMotorista extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
+		JLabel labelLogo = new JLabel("");
+		labelLogo.setBounds(230, 10, 303, 76);
+		labelLogo.setForeground(new Color(0, 0, 0));
+		labelLogo.setBackground(new Color(0, 0, 0));
+		labelLogo.setIcon(new ImageIcon(this.getClass().getResource("/UberLand_LogoF.png")));
+		getContentPane().add(labelLogo);
 		
-		JLabel lblNewLabel_3 = new JLabel("<html>Busca Motorista.<br/>Entre com um dos campos e clique Buscar!</html>");
-		lblNewLabel_3.setBounds(180, 107, 404, 60);
-		lblNewLabel_3.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_3.setFont(new Font("Tahoma", Font.BOLD, 16));
-		contentPane.add(lblNewLabel_3);
+		JLabel lblTitulo = new JLabel("<html>Busca Motorista.<br/>Entre com um dos campos e clique Buscar!</html>");
+		lblTitulo.setBounds(180, 107, 404, 60);
+		lblTitulo.setHorizontalAlignment(SwingConstants.CENTER);
+		lblTitulo.setFont(new Font("Tahoma", Font.BOLD, 16));
+		contentPane.add(lblTitulo);
 		
-		JLabel label = new JLabel("");
-		label.setBounds(230, 10, 303, 76);
-		label.setForeground(new Color(0, 0, 0));
-		label.setBackground(new Color(0, 0, 0));
-		label.setIcon(new ImageIcon(this.getClass().getResource("/UberLand_LogoF.png")));
-		getContentPane().add(label);
+		JLabel lblNome = new JLabel("Nome:");
+		lblNome.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		lblNome.setBounds(210, 190, 46, 14);
+		contentPane.add(lblNome);
 		
-		JLabel lblNewLabel = new JLabel("Nome:");
-		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		lblNewLabel.setBounds(210, 190, 46, 14);
-		contentPane.add(lblNewLabel);
+		txtFieldNome = new JTextField();
+		txtFieldNome.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		txtFieldNome.setColumns(10);
+		txtFieldNome.setBounds(265, 190, 268, 20);
+		contentPane.add(txtFieldNome);
 		
-		textField = new JTextField();
-		textField.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		textField.setColumns(10);
-		textField.setBounds(265, 190, 268, 20);
-		contentPane.add(textField);
+		JLabel lblNomeSocial = new JLabel("Nome Social:");
+		lblNomeSocial.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		lblNomeSocial.setBounds(210, 230, 90, 14);
+		contentPane.add(lblNomeSocial);
 		
-		JLabel lblNomeSocialcomo = new JLabel("Nome Social:");
-		lblNomeSocialcomo.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		lblNomeSocialcomo.setBounds(210, 230, 90, 14);
-		contentPane.add(lblNomeSocialcomo);
+		txtFieldNomeSoc = new JTextField();
+		txtFieldNomeSoc.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		txtFieldNomeSoc.setColumns(10);
+		txtFieldNomeSoc.setBounds(292, 227, 138, 20);
+		contentPane.add(txtFieldNomeSoc);
 		
-		textField_1 = new JTextField();
-		textField_1.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		textField_1.setColumns(10);
-		textField_1.setBounds(292, 227, 138, 20);
-		contentPane.add(textField_1);
+		JLabel lblCPF = new JLabel("CPF:");
+		lblCPF.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		lblCPF.setBounds(210, 280, 46, 14);
+		contentPane.add(lblCPF);
 		
-		JLabel lblCpf = new JLabel("CPF:");
-		lblCpf.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		lblCpf.setBounds(210, 280, 46, 14);
-		contentPane.add(lblCpf);
+		txtFieldCPF = new JTextField();
+		txtFieldCPF.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		txtFieldCPF.setColumns(10);
+		txtFieldCPF.setBounds(265, 280, 133, 20);
+		contentPane.add(txtFieldCPF);
 		
-		textField_2 = new JTextField();
-		textField_2.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		textField_2.setColumns(10);
-		textField_2.setBounds(265, 280, 133, 20);
-		contentPane.add(textField_2);
+		JLabel lblCNH = new JLabel("CNH:");
+		lblCNH.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		lblCNH.setBounds(210, 320, 46, 14);
+		contentPane.add(lblCNH);
 		
-		JLabel lblCnh = new JLabel("CNH:");
-		lblCnh.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		lblCnh.setBounds(210, 320, 46, 14);
-		contentPane.add(lblCnh);
-		
-		textField_3 = new JTextField();
-		textField_3.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		textField_3.setColumns(10);
-		textField_3.setBounds(265, 320, 133, 20);
-		contentPane.add(textField_3);
+		txtFieldCNH = new JTextField();
+		txtFieldCNH.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		txtFieldCNH.setColumns(10);
+		txtFieldCNH.setBounds(265, 320, 133, 20);
+		contentPane.add(txtFieldCNH);
 		
 		JButton btnBuscar = new JButton("Buscar");
+		btnBuscar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				/*
+				 * Ações do botão Buscar Motorista
+				 * Verifica qual campo foi preenchido e realiza busca específica
+				 
+				txtFieldNome.getText(),
+				txtFieldNomeSoc.getText(),
+				txtFieldCPF.getText(),
+				txtFieldCNH.getText(),
+				*/
+			}
+		});
 		btnBuscar.setFont(new Font("Tahoma", Font.PLAIN, 13));
 		btnBuscar.setBounds(337, 366, 89, 23);
 		contentPane.add(btnBuscar);
