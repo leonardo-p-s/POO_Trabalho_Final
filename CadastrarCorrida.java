6605b38eb8a1349de85fb141969e2e9dcb0abc6d
Leonardo <leonardops@gmail.com>
2026-02-19 21:42:17 -0300

    Refactor CadastrarCorrida class and update fields
    Nomes de campos alterados
    Rascunho das ações do botão Pedir Corrida

--- CadastrarCorrida.java
+++ CadastrarCorrida.java
@@ -13,13 +13,17 @@ import javax.swing.JTextField;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class CadastrarCorrida extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-	private JTextField textField;
-	private JTextField textField_1;
+	private JTextField txtFieldOrigem;
+	private JTextField txtFieldDestino;
 
 	/**
 	 * Launch the application.
@@ -50,57 +54,65 @@ public class CadastrarCorrida extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel label = new JLabel("");
-		label.setIcon(new ImageIcon(CadastraVeiculo.class.getResource("/UberLand_LogoF.png")));
-		label.setForeground(Color.BLACK);
-		label.setBackground(Color.BLACK);
-		label.setBounds(230, 10, 303, 76);
-		contentPane.add(label);
+		JLabel labelLogo = new JLabel("");
+		labelLogo.setIcon(new ImageIcon(CadastraVeiculo.class.getResource("/UberLand_LogoF.png")));
+		labelLogo.setForeground(Color.BLACK);
+		labelLogo.setBackground(Color.BLACK);
+		labelLogo.setBounds(230, 10, 303, 76);
+		contentPane.add(labelLogo);
 		
-		JLabel lblNewLabel_3 = new JLabel("Peça um carro!");
-		lblNewLabel_3.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_3.setFont(new Font("Tahoma", Font.BOLD, 16));
-		lblNewLabel_3.setBounds(282, 118, 200, 29);
-		contentPane.add(lblNewLabel_3);
+		JLabel lblTitulo = new JLabel("Peça um carro!");
+		lblTitulo.setHorizontalAlignment(SwingConstants.CENTER);
+		lblTitulo.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblTitulo.setBounds(282, 118, 200, 29);
+		contentPane.add(lblTitulo);
 		
 		JLabel lblOrigem = new JLabel("Origem:");
 		lblOrigem.setFont(new Font("Tahoma", Font.PLAIN, 13));
 		lblOrigem.setBounds(230, 177, 59, 14);
 		contentPane.add(lblOrigem);
 		
-		textField = new JTextField();
-		textField.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		textField.setColumns(10);
-		textField.setBounds(285, 177, 248, 20);
-		contentPane.add(textField);
-		
-		textField_1 = new JTextField();
-		textField_1.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		textField_1.setColumns(10);
-		textField_1.setBounds(285, 210, 248, 20);
-		contentPane.add(textField_1);
+		txtFieldOrigem = new JTextField();
+		txtFieldOrigem.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		txtFieldOrigem.setColumns(10);
+		txtFieldOrigem.setBounds(285, 177, 248, 20);
+		contentPane.add(txtFieldOrigem);
 		
 		JLabel lblDestino = new JLabel("Destino:");
 		lblDestino.setFont(new Font("Tahoma", Font.PLAIN, 13));
 		lblDestino.setBounds(230, 210, 59, 14);
 		contentPane.add(lblDestino);
 		
+		txtFieldDestino = new JTextField();
+		txtFieldDestino.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		txtFieldDestino.setColumns(10);
+		txtFieldDestino.setBounds(285, 210, 248, 20);
+		contentPane.add(txtFieldDestino);
+		
+		JLabel lblEscolhaCarro = new JLabel("Escolha:");
+		lblEscolhaCarro.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		lblEscolhaCarro.setBounds(230, 241, 59, 14);
+		contentPane.add(lblEscolhaCarro);
+		
+		JComboBox comboBoxTipoCarro = new JComboBox();
+		comboBoxTipoCarro.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		comboBoxTipoCarro.setModel(new DefaultComboBoxModel(new String[] {"UberX", "UberConfort", "UberBlack"}));
+		comboBoxTipoCarro.setBounds(282, 241, 105, 20);
+		contentPane.add(comboBoxTipoCarro);
+		
 		JButton btnChamar = new JButton("Pedir");
+		btnChamar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				/* Ações do Botão Pedir (criar) Corrida
+				 * Usuario s, String or, String dest, String d, String h
+				Corrida c = new Corrida(UsuarioAtual, txtFieldOrigem.getText(), txtFieldDestino.getText(), LocalDate.now(), LocalTime.now());
+            	Corridas.criar(c);???
+            	*/
+			}
+		});
 		btnChamar.setFont(new Font("Tahoma", Font.PLAIN, 13));
 		btnChamar.setBounds(337, 272, 89, 23);
 		contentPane.add(btnChamar);
-		
-		JLabel lblEscolha = new JLabel("Escolha:");
-		lblEscolha.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		lblEscolha.setBounds(230, 241, 59, 14);
-		contentPane.add(lblEscolha);
-		
-		JComboBox comboBox = new JComboBox();
-		comboBox.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		comboBox.setModel(new DefaultComboBoxModel(new String[] {"UberX", "UberConfort", "UberBlack"}));
-		comboBox.setBounds(282, 241, 105, 20);
-		contentPane.add(comboBox);
-
 	}
 
 }
