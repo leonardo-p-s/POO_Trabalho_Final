f699ba728cfc3bdabd5394c3b7bf789e9bc8767b
Leonardo <leonardops@gmail.com>
2026-02-21 16:31:43 -0300

    Refactor TelaInicio class and update frame variables

--- TelaInicio.java
+++ TelaInicio.java
@@ -31,7 +31,7 @@ import java.awt.event.ActionEvent;
 
 public class TelaInicio {
 
-	private JFrame frmCadastro;
+	private JFrame frmLogin;
 	private JTextField txtFieldUsuario;
 	private JPasswordField passwordField;
 
@@ -43,7 +43,7 @@ public class TelaInicio {
 			public void run() {
 				try {
 					TelaInicio window = new TelaInicio();
-					window.frmCadastro.setVisible(true);
+					window.frmLogin.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -62,41 +62,41 @@ public class TelaInicio {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frmCadastro = new JFrame();
-		frmCadastro.setIconImage(Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("/UberLand_Icon.png")));
-		frmCadastro.setTitle("UberLand");
-		frmCadastro.setBounds(100, 100, 780, 735);
-		frmCadastro.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frmCadastro.getContentPane().setLayout(null);
+		frmLogin = new JFrame();
+		frmLogin.setIconImage(Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("/UberLand_Icon.png")));
+		frmLogin.setTitle("UberLand");
+		frmLogin.setBounds(100, 100, 780, 735);
+		frmLogin.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmLogin.getContentPane().setLayout(null);
 		
 		JLabel lblLogo = new JLabel("");
 		lblLogo.setIcon(new ImageIcon(this.getClass().getResource("/UberLand_Pic1.png")));
 		lblLogo.setHorizontalAlignment(SwingConstants.CENTER);
 		lblLogo.setBounds(10, 11, 423, 674);
-		frmCadastro.getContentPane().add(lblLogo);
+		frmLogin.getContentPane().add(lblLogo);
 		
 		JLabel lblTitulo = new JLabel("Olá! Faça seu login!");
 		lblTitulo.setFont(new Font("Tahoma", Font.BOLD, 16));
-		lblTitulo.setBounds(509, 39, 200, 29);
-		frmCadastro.getContentPane().add(lblTitulo);
+		lblTitulo.setBounds(497, 40, 200, 29);
+		frmLogin.getContentPane().add(lblTitulo);
 		
 		JLabel lblUsuario = new JLabel("Usuário:");
-		lblUsuario.setBounds(485, 104, 50, 14);
-		frmCadastro.getContentPane().add(lblUsuario);
+		lblUsuario.setBounds(473, 105, 50, 14);
+		frmLogin.getContentPane().add(lblUsuario);
 		
 		txtFieldUsuario = new JTextField();
 		txtFieldUsuario.setToolTipText("email");
-		txtFieldUsuario.setBounds(537, 101, 151, 20);
-		frmCadastro.getContentPane().add(txtFieldUsuario);
+		txtFieldUsuario.setBounds(522, 102, 151, 20);
+		frmLogin.getContentPane().add(txtFieldUsuario);
 		txtFieldUsuario.setColumns(10);
 		
 		JLabel lblSenha = new JLabel("Senha:");
-		lblSenha.setBounds(489, 132, 46, 14);
-		frmCadastro.getContentPane().add(lblSenha);
+		lblSenha.setBounds(477, 133, 46, 14);
+		frmLogin.getContentPane().add(lblSenha);
 
 		passwordField = new JPasswordField();
-		passwordField.setBounds(537, 129, 151, 20);
-		frmCadastro.getContentPane().add(passwordField);
+		passwordField.setBounds(522, 130, 151, 20);
+		frmLogin.getContentPane().add(passwordField);
 		
 		JButton btnLogin = new JButton("Login");
 		btnLogin.addActionListener(new ActionListener() {
@@ -105,93 +105,92 @@ public class TelaInicio {
 				if(login.isEmpty())
 					JOptionPane.showMessageDialog(null,  "Favor digitar um login!", "Login vazio", JOptionPane.WARNING_MESSAGE);
 				/*Ações botão Login
-				Verifica se Login Existe
-					verifica se é Motorista
+				Verifica se Login é Motorista
 						Pagina Motorista
 					verifica se é Usuário
 						Página Usuário
 				txtFieldUsuario.getText()
+				Sessao.getInstancia().setUsuario(new Usuario("Leonardo"));
 				*/
 				JOptionPane.showMessageDialog(null,  "Usuário não encontrado!", "Atenção", JOptionPane.WARNING_MESSAGE);
 			}
 		});
-		btnLogin.setBounds(568, 160, 89, 23);
-		frmCadastro.getContentPane().add(btnLogin);
+		btnLogin.setBounds(553, 161, 89, 23);
+		frmLogin.getContentPane().add(btnLogin);
 		
 		JSeparator separator_1_1 = new JSeparator();
-		separator_1_1.setBounds(467, 246, 80, 1);
-		frmCadastro.getContentPane().add(separator_1_1);
+		separator_1_1.setBounds(455, 247, 80, 1);
+		frmLogin.getContentPane().add(separator_1_1);
 		
 		JLabel lblQuebraCadastUsu = new JLabel("ou cadastrar usuário");
 		lblQuebraCadastUsu.setHorizontalAlignment(SwingConstants.CENTER);
-		lblQuebraCadastUsu.setBounds(552, 239, 120, 14);
-		frmCadastro.getContentPane().add(lblQuebraCadastUsu);
+		lblQuebraCadastUsu.setBounds(537, 240, 120, 14);
+		frmLogin.getContentPane().add(lblQuebraCadastUsu);
 		
 		JSeparator separator_1_2 = new JSeparator();
-		separator_1_2.setBounds(675, 246, 80, 1);
-		frmCadastro.getContentPane().add(separator_1_2);
+		separator_1_2.setBounds(663, 247, 80, 1);
+		frmLogin.getContentPane().add(separator_1_2);
 		
 		JButton btnCriaContaUsuario = new JButton("Criar Conta");
 		btnCriaContaUsuario.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				/*
-				Ações Botão Cria Conta Usuário
-					Verifica se Usuário foi teclado
-						Página Cadastrar Usuário
-					Senão
-						Erro, Pede que entre com usuário e senha
-				txtFieldUsuario.getText()
-				*/
+				// Ações Botão Cria Conta Usuário
+				frmLogin.setVisible(false);
+				CadastraUsuario.main(null);
+				//frmCadastroDeUsurio.setVisible(false); 
 			}
 		});
-		btnCriaContaUsuario.setBounds(562, 264, 100, 23);
-		frmCadastro.getContentPane().add(btnCriaContaUsuario);
+		btnCriaContaUsuario.setBounds(547, 274, 100, 23);
+		frmLogin.getContentPane().add(btnCriaContaUsuario);
 		
 		JSeparator separator_2_1 = new JSeparator();
-		separator_2_1.setBounds(467, 334, 70, 1);
-		frmCadastro.getContentPane().add(separator_2_1);
+		separator_2_1.setBounds(455, 335, 70, 1);
+		frmLogin.getContentPane().add(separator_2_1);
 		
 		JLabel lblQuebraCadastMoto = new JLabel("ou trabalhe com a gente");
 		lblQuebraCadastMoto.setHorizontalAlignment(SwingConstants.CENTER);
-		lblQuebraCadastMoto.setBounds(542, 327, 140, 14);
-		frmCadastro.getContentPane().add(lblQuebraCadastMoto);
+		lblQuebraCadastMoto.setBounds(527, 328, 140, 14);
+		frmLogin.getContentPane().add(lblQuebraCadastMoto);
 		
 		JSeparator separator_2_2 = new JSeparator();
-		separator_2_2.setBounds(685, 334, 70, 1);
-		frmCadastro.getContentPane().add(separator_2_2);
+		separator_2_2.setBounds(673, 335, 70, 1);
+		frmLogin.getContentPane().add(separator_2_2);
 		
 		JButton btnCadastrarMotorista = new JButton("Cadastrar como motorista");
 		btnCadastrarMotorista.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				/*
-				Ações Botão Cadastrar Motorista
-					Verifica se Usuário foi teclado
-						Página Cadastrar Motorista
-					Senão
-						Erro, Pede que entre com usuário e senha
-				txtFieldUsuario.getText()
-				*/
+				// Ações Botão Cria Conta Usuário
+				frmLogin.setVisible(false);
+				CadastroMotorista.main(null);
+				//frmCadastroDeUsurio.setVisible(falsse); 
 			}
 		});
-		btnCadastrarMotorista.setBounds(517, 354, 190, 23);
-		frmCadastro.getContentPane().add(btnCadastrarMotorista);
+		btnCadastrarMotorista.setBounds(502, 364, 190, 23);
+		frmLogin.getContentPane().add(btnCadastrarMotorista);
 		
 		JSeparator separator_3_1 = new JSeparator();
-		separator_3_1.setBounds(467, 438, 65, 1);
-		frmCadastro.getContentPane().add(separator_3_1);
+		separator_3_1.setBounds(455, 439, 65, 1);
+		frmLogin.getContentPane().add(separator_3_1);
 
 		JLabel lblAcessoRestrito = new JLabel("acesso restrito UberLand");
 		lblAcessoRestrito.setHorizontalAlignment(SwingConstants.CENTER);
-		lblAcessoRestrito.setBounds(540, 431, 145, 14);
-		frmCadastro.getContentPane().add(lblAcessoRestrito);
+		lblAcessoRestrito.setBounds(525, 432, 145, 14);
+		frmLogin.getContentPane().add(lblAcessoRestrito);
 		
 		JSeparator separator_3_2 = new JSeparator();
-		separator_3_2.setBounds(690, 438, 65, 1);
-		frmCadastro.getContentPane().add(separator_3_2);
+		separator_3_2.setBounds(678, 439, 65, 1);
+		frmLogin.getContentPane().add(separator_3_2);
 
 		JButton btnGerenciar = new JButton("Gerenciar");
-		btnGerenciar.setBounds(560, 450, 105, 23);
-		frmCadastro.getContentPane().add(btnGerenciar);
+		btnGerenciar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// Ações Botão Cria Conta Usuário
+				frmLogin.setVisible(false);
+				Gerenciar.main(null);
+			}
+		});
+		btnGerenciar.setBounds(545, 460, 105, 23);
+		frmLogin.getContentPane().add(btnGerenciar);
 
 		
 	}
