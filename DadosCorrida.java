d4358303e9a6012c2cef109d56dc417552579979
Leonardo <leonardops@gmail.com>
2026-02-27 17:33:22 -0300

    Update DadosCorrida.java

--- DadosCorrida.java
+++ DadosCorrida.java
@@ -5,17 +5,18 @@ import java.util.ArrayList;
 
 public class DadosCorrida {
 
-    private ArrayList<Corrida> vetorCorridas;
+    private static ArrayList<Corrida> vetorCorridas;
 
-    public void inicializaCorridas() {
+    public static void inicializaCorridas() {
         if (vetorCorridas == null) { // caso o vetor de corridas ainda não exista...
             vetorCorridas = new ArrayList<Corrida>(); // inicializamos um vetor para começar os cadastros das
                                                       // corridas...
         }
     }
 
-    public void cadastraCorrida(Corrida c) {
+    public static void cadastraCorrida(Corrida c) {
         vetorCorridas.add(c);
+        salvarArquivoCorridas();
         System.out.println("Corrida adicionada com sucesso!"); // exibimos uma mensagem dizendo que o processo de
                                                                // adicionar deu certo!
     }
@@ -34,11 +35,11 @@ public class DadosCorrida {
         return null; // caso não encontrarmos a corrida desejada, simplesmente retornamos nulo.
     }
 
-    public Corrida criarCorrida(String origem, String destino, Usuario solicitante) {
+    public Corrida criarCorrida(String origem, String destino, String CatVeic, Usuario solicitante) {
         String dataAtual = LocalDate.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
         String horaAtual = LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm"));
 
-        Corrida novaCorrida = new Corrida(solicitante, origem, destino, dataAtual, horaAtual);
+        Corrida novaCorrida = new Corrida(solicitante, origem, destino, CatVeic, dataAtual, horaAtual);
 
         vetorCorridas.add(novaCorrida);
 
@@ -53,6 +54,7 @@ public class DadosCorrida {
 
             if (m.equals(c.getMotorista())) { // se o id do motorista pertencer a alguma das corridas cadastradas...
                 vetorCorridas.remove(c); // removemos a corrida do vetor de corridas...
+                salvarArquivoCorridas();
                 System.out.println("Corrida removida com sucesso!"); // mensagem que informa que o processo deu certo
                 break; // e então, solicitamos para que o comando de laço se encerre, já que
                        // encontramos a corrida desejada e já a removemos do array...
@@ -74,12 +76,93 @@ public class DadosCorrida {
         // OBS: esse processo acontecerá com todas as corridas presentes no array, até
         // ele acabar com aquelas que estão cadastradas
     }
+    
+    public static Corrida corridaEmAndamentoUsuario() {
+    	Object u = Sessao.getInstancia().getUser();
+    	for (Corrida c : vetorCorridas) {
+    		if (u.equals(c.getSolicitante())) {
+    			if (!c.getStatusCorrida().equals("Finalizada")) {
+    				return c;
+    			}
+    		}
+    	}
+    	return null;
+    }
+    
+    public static String dadosCorridaEmAndamentoUsuario() {
+    	Object u = Sessao.getInstancia().getUser();
+    	for (Corrida c : vetorCorridas) {
+    		if (u.equals(c.getSolicitante())) {
+    			if (!c.getStatusCorrida().equals("Finalizada")) {
+    				String or = c.getOrigem();
+					String dest = c.getDestino();
+					String status = c.getStatusCorrida();
+					String mot = "Aguarde!";
+					String veic = "Aguarde!";
+					String valor = "Aguarde!";
+					if(!status.equals("Solicitada")) {
+						mot = c.getMotorista().getNomeSocial();
+						veic = c.getVeiculo().getMarca() + "-"+c.getVeiculo().getModelo();
+						valor = "R$ "+ c.getValorTotalViagem();
+					}
+	    				return("<html><b>Origem:</b> "	+ or +
+	    						"<br><b>Destino:</b> "	+ dest +
+	    						"<br><b>Nome Motorista:</b> "	+mot+
+	    						"<br><b>Veículo:</b> "	+ veic+
+	    						"<br><b>Valor Total:</b> "	+ valor +
+	    						"<br><b>Status:</b> "	+status+
+	    						" </html>");
+    				}
+    			}
+    		}
+    	return "";
+    }
+    
+    public static Corrida corridaEmAndamentoMotorista() {
+    	Object u = Sessao.getInstancia().getUser();
+    	for (Corrida c : vetorCorridas) {
+    		if(!c.getStatusCorrida().equals("Solicitada")) {
+	    		if (u.equals(c.getMotorista())) {
+	    			if (!c.getStatusCorrida().equals("Finalizada")) {
+	    				return c;
+	    			}
+	    		}
+    		}
+    	}
+    	return null;
+    }
+    
+    public static String dadosCorridaEmAndamentoMotorista() {
+    	Object u = Sessao.getInstancia().getUser();
+    	for (Corrida c : vetorCorridas) {
+    		if(!c.getStatusCorrida().equals("Solicitada")) {
+	    		if (u.equals(c.getMotorista())) {
+	    			if (!c.getStatusCorrida().equals("Finalizada")) {
+	    				String or = c.getOrigem();
+						String dest = c.getDestino();
+						String status = c.getStatusCorrida();
+						String solic = c.getSolicitante().getNome();
+						String veic = c.getVeiculo().getMarca() + "-"+c.getVeiculo().getModelo();
+						String valor = "R$ "+ c.getValorTotalViagem();
+	    				return("<html><b>Origem:</b> "	+ or +
+	    						"<br><b>Destino:</b> "	+ dest +
+	    						"<br><b>Nome Passageiro:</b> "	+solic+
+	    						"<br><b>Veículo:</b> "	+ veic+
+	    						"<br><b>Valor Total:</b> "	+ valor +
+	    						"<br><b>Status:</b> "	+status+
+	    						" </html>");
+	    			}
+	    		}
+    		}
+    	}
+    	return "";
+    }
 
-    public void salvarArquivoCorridas() {
+    public static void salvarArquivoCorridas() {
         Persist.gravar(vetorCorridas, "corridas.dat");
     }
 
-    public void recuperarArquivoCorridas() {
+    public static void recuperarArquivoCorridas() {
         Object obj = Persist.recuperar("corridas.dat");
 
         if (obj != null) {
